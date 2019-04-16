d279e935cb6411b96f39323302d30309680d2a62
dragonfly01 <por.nattapat.01@gmail.com>
2019-04-17 03:58:02 +0700

    Add memory usage and limit memory

--- RunningDocker.sh
+++ RunningDocker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# : [name] [time]
+# : [name] [memory] [time]
 
 NAME=$1
 shift
+MEM=$1
+shift
 
 echo "Starting docker."
-docker run --rm --name NAME -it -v $(pwd):/app cpprun app/Processing.sh $@
+docker run --name $NAME -it -m $MEM --memory-swap="${MEM}" -v $(pwd):/app cpprun app/Processing.sh $@
+# docker inspect $NAME
+docker kill $NAME > /dev/null 2>&1
+docker rm $NAME
 echo "Ending docker process."
 
 exit
